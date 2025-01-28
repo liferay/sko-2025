5a6b9b6a44eb49bd66e8d7a98751632b40d56fe9
jhanda <jeffrey.handa@liferay.com>
2025-01-27 22:25:48 -0500

    Adding the artifacts for Exercise 8 - Adding an action to the Distributor Management App

---
+++ exercises/exercise-8/07-action-classes/TaskExecutorConfig.java
@@ -0,0 +1,40 @@
+package com.claritysolutions.distributor.mgmt.actions;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ * Creates the task executor which will be processing the user creation requests.
+ *
+ * @author dnebing
+ */
+@Configuration
+public class TaskExecutorConfig {
+
+	@Bean
+	public TaskExecutor taskExecutor() {
+
+		// create a thread pool for the task executor
+
+		ThreadPoolTaskExecutor threadPoolTaskExecutor =
+			new ThreadPoolTaskExecutor();
+
+		// initialize to some reasonable values
+
+		threadPoolTaskExecutor.setCorePoolSize(0);
+		threadPoolTaskExecutor.setMaxPoolSize(10);
+		threadPoolTaskExecutor.setQueueCapacity(100);
+		threadPoolTaskExecutor.setThreadNamePrefix("CreateUserTaskExecutor-");
+
+		// initialize the task executor
+
+		threadPoolTaskExecutor.initialize();
+
+		// return the task executor
+
+		return threadPoolTaskExecutor;
+	}
+
+}
